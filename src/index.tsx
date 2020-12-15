bc08d7f61367604d2993ab65d7d0de349e1e0b27
Etienne Dldc <e.deladonchamps@gmail.com>
2020-12-15 23:34:34 +0100

    Fix emotion

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,29 @@
 import './style';
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorkerRegistration';
 import store from './store';
 import reportWebVitals from './reportWebVitals';
+import { App } from './components/App';
+import { Provider } from './select';
+import { Colors } from '@blueprintjs/core';
+import { Global, css } from '@emotion/react';
 
+import './index.css';
 import 'normalize.css';
 import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 import '@blueprintjs/core/lib/css/blueprint.css';
 
-import { App } from './components/App';
-import { Provider } from './select';
-
 ReactDOM.render(
   <Provider store={store}>
+    <Global
+      styles={css`
+        body {
+          background-color: ${Colors.DARK_GRAY3};
+        }
+      `}
+    />
     <App />
   </Provider>,
   document.getElementById('root') as HTMLElement
